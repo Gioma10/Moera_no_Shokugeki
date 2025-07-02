527a6d7ee4478b44c0bcf55348e46467ef5ae9e8
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-07-02 20:03:53 +0200

    switcher animation completed

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
 import { IconType } from "react-icons";
 import { FaBook } from "react-icons/fa";
 import { FaPlusCircle } from "react-icons/fa";
 import { FaTrophy } from "react-icons/fa";
 
 const Switcher = () => {
-  const [activeSwitch, setActiveSwitch] = useState("");
+  const [activeSwitch, setActiveSwitch] = useState<string>("recipes");
+  const bgSwitch = useRef(null);
+
+  useEffect(() => {
+    let yValue;
+
+    if (activeSwitch === "recipes") {
+      yValue = 0;
+    } else if (activeSwitch === "classification") {
+      yValue = "4.5rem"; // ad esempio 4.5rem in px
+    } else if (activeSwitch === "plus") {
+      yValue = "9rem";
+    } else {
+      yValue = 0;
+    }
+    if (bgSwitch.current) {
+      gsap.to(bgSwitch.current, {
+        y: yValue,
+        duration: 0.4,
+        ease: "power3.inOut",
+      });
+    }
+  }, [activeSwitch]);
+
   const icons: { title: string; Icon: IconType }[] = [
     {
       title: "recipes",
@@ -20,16 +44,13 @@ const Switcher = () => {
       Icon: FaPlusCircle,
     },
   ];
+
   return (
-    <div  className="border overflow-hidden flex flex-col absolute hover:bg-gray-500/ top-1/2 right-0 -translate-y-1/2 rounded-4xl">
+    <div className="border overflow-hidden flex flex-col absolute hover:bg-gray-500/ top-1/2 right-0 -translate-y-1/2 rounded-4xl">
       {icons.map((item, index) => {
         return (
           <div
-            className={`p-5 transition-all duration-500 cursor-pointer ${
-              item.title === activeSwitch
-                ? "bg-[#fab18a]"
-                : "hover:bg-gray-500/30"
-            }`}
+            className={`w-18 h-18 transition-all duration-300 flex items-center justify-center cursor-pointer hover:text-black`}
             key={index}
             onClick={() => setActiveSwitch(item.title)}
           >
@@ -37,6 +58,10 @@ const Switcher = () => {
           </div>
         );
       })}
+      <div
+        ref={bgSwitch}
+        className="absolute top-0 w-18 h-18 left-0 bg-[#fab18a] -z-20"
+      ></div>
     </div>
   );
 };
