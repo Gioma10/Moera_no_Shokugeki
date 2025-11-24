d872cf24d5ab22f893392bc137a18792ac599160
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-11-24 22:36:09 +0100

    feat: add fetch function and query to get recipes

--- client/src/api/recipes.ts
+++ client/src/api/recipes.ts
@@ -1,18 +1,22 @@
 import { RecipeFormData } from "@/types/recipes";
+const localurl = "http://localhost:8080"
 
 // Get recipes
-
-export const getRecipes = async ()=>{
-  
+export const getRecipes = async (): Promise<RecipeFormData[]>=>{
+  const res = await fetch(`${localurl}/api/recipes`, {
+    method: "GET",
+  })
+  if (!res.ok) throw new Error("Errore on recipes visualization");
+  return res.json()
 }
 
 // Create a recipe
 export const createRecipe = async (newRecipe: RecipeFormData) => {
-  const res = await fetch("http://localhost:8080/api/recipes", {
+  const res = await fetch(`${localurl}/api/recipes`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(newRecipe),
   });
-  if (!res.ok) throw new Error("Errore nella creazione della ricetta");
+  if (!res.ok) throw new Error("Errore on recipe creation");
   return res.json();
 };
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,40 @@
-export default function Home() {
+"use client";
 
+import { getRecipes } from "@/api/recipes";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import { match } from "ts-pattern";
+
+export default function Home() {
+  const recipesQueryResult = useQuery({
+    queryKey: ["recipes"],
+    queryFn: getRecipes,
+  });
 
   return (
     <div className="flex h-screen items-center justify-center">
-
+      {match(recipesQueryResult)
+        .with({ status: "error" }, () => <div>Error...</div>)
+        .with({ status: "pending" }, () => <div>Skeleton...</div>)
+        .with({ status: "success" }, () => (
+          <div>
+            {recipesQueryResult.data?.map((recipe) => {
+              return (
+                <Card key={recipe.title}>
+                  <CardHeader className="w-full h-12">
+                    <Image src={recipe.image ?? ''} alt="Recipe Image" fill/>
+                  </CardHeader>
+                  <CardContent className="">
+                    <h3>{recipe.title}</h3>
+                    <span>{recipe.description}</span>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        ))
+        .exhaustive()}
     </div>
   );
 }
