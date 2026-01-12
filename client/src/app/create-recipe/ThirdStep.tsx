8622ab167ab749c0d88d01b2a2e1480d9950f518
Giovanni Mauro <giovi.mauro03@gmail.com>
2026-01-12 13:46:49 +0100

    chore add methods field

--- client/src/app/create-recipe/ThirdStep.tsx
+++ client/src/app/create-recipe/ThirdStep.tsx
@@ -2,6 +2,7 @@ import z from "zod";
 import { RecipeSchema } from "@/types/recipes";
 import { UseFormReturn } from "react-hook-form";
 import { Note, Preparation } from "@/components/CreateRecipe/PreparationAndNote";
+import { Method } from "@/components/CreateRecipe/Method";
 
 export const ThirdStep = ({
   form,
@@ -9,13 +10,16 @@ export const ThirdStep = ({
   form: UseFormReturn<z.infer<typeof RecipeSchema>>;
 }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-5">
       <div className="flex gap-5">
         <Preparation name="preparation" control={form.control} />
         <Note name="note" control={form.control}/>
       </div>
       <div className="flex gap-5">
-        
+        <div className="flex-1">
+
+        <Method name="method" control={form.control}/>
+        </div>
         <Note name="note" control={form.control}/>
       </div>
     </div>
